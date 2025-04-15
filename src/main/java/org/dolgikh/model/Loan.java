7a3599456fa3f9b7af79067d0a3a2dcb851a9afc
rdolgikh <raywen.w@gmail.com>
2025-04-16 02:17:55 +0300

    add core models (Book, Reader, Loan)

---
+++ src/main/java/org/dolgikh/model/Loan.java
@@ -0,0 +1,61 @@
+package org.dolgikh.model;
+
+import java.util.Date;
+
+public class Loan {
+    private int id;
+    private Book book;
+    private Reader reader;
+    private Date loanDate;
+    private Date returnDate;
+
+    public Loan() {}
+
+    public Loan(int id, Book book, Reader reader, Date loanDate, Date returnDate) {
+        this.id = id;
+        this.book = book;
+        this.reader = reader;
+        this.loanDate = loanDate;
+        this.returnDate = returnDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Reader getReader() {
+        return reader;
+    }
+
+    public void setReader(Reader reader) {
+        this.reader = reader;
+    }
+
+    public Date getLoanDate() {
+        return loanDate;
+    }
+
+    public void setLoanDate(Date loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+}
---
+++ src/main/java/org/dolgikh/model/Reader.java
@@ -0,0 +1,49 @@
+package org.dolgikh.model;
+
+public class Reader {
+    private int id;
+    private String fullName;
+    private String email;
+    private String phone;
+
+    public Reader() {}
+
+    public Reader(int id, String fullName, String email, String phone) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
