7594f7d1d12b89cb587e9cac3a138c049561b366
rdolgikh <raywen.w@gmail.com>
2025-04-16 02:04:54 +0300

    setup project with Maven and postgres dependency

---
+++ src/main/java/org/dolgikh/App.java
@@ -0,0 +1,13 @@
+package org.dolgikh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
