217d4cb5b0eff38e1de779c61dc4258cfc36dc0b
rdolgikh <raywen.w@gmail.com>
2025-04-23 23:01:58 +0300

    move database connection credentials to application.yaml

--- src/main/java/org/dolgikh/util/DatabaseConnector.java
+++ src/main/java/org/dolgikh/util/DatabaseConnector.java
@@ -1,15 +1,41 @@
 package org.dolgikh.util;
 
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Map;
 
 public class DatabaseConnector {
-    private static final String URL = "jdbc:postgresql://localhost:5432/java-library-app";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "1234";
+    private static String url;
+    private static String user;
+    private static String password;
+
+    static {
+        loadConfig();
+    }
+
+    private static void loadConfig() {
+        try (InputStream in = DatabaseConnector.class.getClassLoader().getResourceAsStream("application.yaml")) {
+            if (in == null) {
+                throw new RuntimeException("Cannot find application.yaml in resources");
+            }
+
+            Yaml yaml = new Yaml();
+            Map<String, Object> config = yaml.load(in);
+            Map<String, String> db = (Map<String, String>) config.get("database");
+
+            url = db.get("url");
+            user = db.get("user");
+            password = db.get("password");
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load database configuration", e);
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(url, user, password);
     }
 }
